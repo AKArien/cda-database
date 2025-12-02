1bf62b9d055a39cd488c8c30805421e20cc8ba80
Hue <skeptikal.monke@gmail.com>
2025-12-02 15:44:33 +0100

    naming in permissions and validy checking trigger

--- migrations/003-permissions-management.sql
+++ migrations/003-permissions-management.sql
@@ -1,19 +1,19 @@
 begin;
 select _v.register_patch('003-permissions-management', ARRAY['002-auth'], NULL);
 
--- create schema permissions; 
+-- create schema permissions; -- todo : determine if we want that or not
 
 create table accesses_group (
 	id serial primary key,
 	name text unique not null,
 	description text,
-	master_group int references accesses_group(id),
+	master_group int references accesses_group(id)
 );
 
 create table access_in_group (
-	access int references accesses(id),
-	group int references accesses(id),
-	primary key (access, group),
+	access int references auth.accesses(id),
+	a_group int references auth.accesses(id),
+	primary key (access, a_group)
 );
 
 /*
@@ -34,39 +34,39 @@ create type permissions_verb as enum (
 	'grant',
 	'grant_propagate',
 	'revoke',
-	'revoke_propagate',
+	'revoke_propagate'
 );
 
-create type permission_owner as enum (
-	'group',
+create type permissions_owner as enum (
 	'access',
+	'a_group'
 );
 
-create type permission_target as enum (
+create type permissions_target as enum (
 	'access',
-	'group',
+	'a_group',
 	'site',
 	'gateway',
-	'watcher',
+	'watcher'
 );
 
 -- members specific to accesses. applied to a group, it gives the permissoin to every access in said group
 create type permissions_members_accesses as enum (
 	'lifetime',
 	'session_time',
-	'change_pass',
+	'change_pass'
 );
 
 --  members specific to on-site equipment. applies uniformally and recursively for sites, gateways and watchers
 create type permissions_members_on_site as enum (
 	'info',
 	'location',
-	'reports',
+	'reports'
 );
 
-create type permission_members as enum (
+create type permissions_members as enum (
 	'permissions_members_accesses',
-	'permissions_members_on_site',
+	'permissions_members_on_site'
 );
 
 create table permissions (
@@ -76,19 +76,82 @@ create table permissions (
 	-- hereby declare that
 	reciever int,
 	-- of type
-	reciever_type permission_owner,
+	reciever_type permissions_owner,
 	-- is to be trusted with
-	action permission_verb, --ing
+	action permissions_verb, --ing
 	-- but not / and delegating this trust
 	propagate bool,
 	-- over the 
-	member permission_member,
+	member permissions_members,
 	-- of
 	target int,
 	-- of type
-	target_type permission_target,
+	target_type permissions_target,
 
-	primary key (reciever, reciever_type, action, propagate, member, target, target_type),
+	primary key (reciever, reciever_type, action, propagate, member, target, target_type)
 );
+
+create function check_permissions_validity()
+returns trigger as $$
+begin
+	-- verify receiver is valid
+	if NEW.reciever_type = 'access' then
+		if not exists (
+			select id from auth.accesses where id = NEW.reciever
+		) then
+			raise exception 'Invalid input data: reciever invalid';
+		end if;
+	elsif NEW.reciever_type = 'a_group' then
+		if not exists (
+			select id from accesses_group where id = NEW.reciever
+		) then
+			raise exception 'Invalid input data: reciever invalid';
+		end if;
+	end if;
+
+	-- verify target is valid
+	if NEW.target_type = 'access' then
+		if not exists (
+			select id from auth.accesses where id = NEW.target
+		) then
+			raise exception 'Invalid input data: target invalid';
+		end if;
+	elsif NEW.target_type = 'a_group' then
+		if not exists (
+			select id from accesses_group where id = NEW.target
+		) then
+			raise exception 'Invalid input data: target invalid';
+		end if;
+	elsif NEW.target_type = 'site' then
+		if not exists (
+			select id from sites where id = NEW.target
+		) then
+			raise exception 'Invalid input data: target invalid';
+		end if;
+	elsif NEW.target_type = 'gateway' then
+		if not exists (
+			select id from gateways where id = NEW.target
+		) then
+			raise exception 'Invalid input data: target invalid';
+		end if;
+	elsif NEW.target_type = 'watcher' then
+		if not exists (
+			select id from watchers where id = NEW.target
+		) then
+			raise exception 'Invalid input data: target invalid';
+		end if;
+	end if;
+
+	-- permissions checks occur in row level security
+	return NEW;
+end;
+$$ language plpgsql;
+
+create trigger enforce_check_permissions_validity
+	before insert or update
+	on permissions
+	for each row
+	execute function check_permissions_validity()
+;
 
 commit;
