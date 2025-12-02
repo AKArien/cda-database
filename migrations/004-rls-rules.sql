fc4fa5ea93eba3e572481098e96a3f908fdf161a
Hue <skeptikal.monke@gmail.com>
2025-12-02 15:46:50 +0100

    draft of rls

--- migrations/004-rls-rules.sql
+++ migrations/004-rls-rules.sql
@@ -6,56 +6,76 @@ alter table sites enable row level security;
 alter table gateways enable row level security;
 alter table watchers enable row level security;
 -- alter table reports enable row level security;
+alter table permissions enable row level security;
 
 -- base permissions
 grant select on sites to web;
 grant select on gateways to web;
 grant select on watchers to web;
-
-grant select on permissions to web ;
+-- grant select on reports to web;
+grant select on permissions to web;
 
 -- rls rules
 
+-- read own permissions and permissions over self
+create policy own_permissions_read on permissions to web
+using (
+	( -- user is the reciever
+		reciever_type = 'access'
+		AND
+		reciever = (current_setting('request.jwt.claims', true)::json->>'id')::int
+	)
+	OR
+	( -- user is in a reciever group
+		reciever_type = 'a_group'
+		AND
+		exists (
+			select access from access_in_group
+			where access = (current_setting('request.jwt.claims', true)::json->>'id')::int
+		)
+	)
+	OR
+	( -- user is the target
+		target_type = 'access'
+		AND
+		target = (current_setting('request.jwt.claims', true)::json->>'id')::int
+	)
+	OR
+	( -- user is in a target group
+		target_type = 'a_group'
+		AND
+		exists (
+			select access from access_in_group
+			where access = (current_setting('request.jwt.claims', true)::json->>'id')::int
+		)
+	)
+);
+
 -- read data monitoring elements
-create policy read_permissions on sites to web
+create policy permissions_read on sites to web
 using (
-	id = (
-		select site from auth.sites_permissions
-		where access::text = current_setting('request.jwt.claims', true)::json->>'id'
+	id in (
+		select reciever from permissions
+		where -- sites where
+			target_type = 'site'
+			and
+			(
+				( -- the user has rights
+					reciever_type = 'access'
+					and
+					reciever = (current_setting('request.jwt.claims', true)::json->>'id')::int
+				)
+				or
+				( -- the user is in a group that has rights
+					reciever_type = 'a_group'
+					and
+					exists (
+						select access from access_in_group
+						where access = (current_setting('request.jwt.claims', true)::json->>'id')::int
+					)
+				)
+			)
 	)
 );
-
-create policy read_permissions on gateways to web
-using (
-	id = (
-		select gateway from auth.gateways_permissions
-		where access::text = current_setting('request.jwt.claims', true)::json->>'id'
-	)
-);
-
-create policy read_permissions on watchers to web
-using (
-	id = (
-		select site from auth.watchers_permissions
-		where access::text = current_setting('request.jwt.claims', true)::json->>'id'
-	)
-);
-
--- read own permissions
-create policy read_own_permissions on auth.sites_permissions to web
-using (
-	access::text = current_setting('request.jwt.claims', true)::json->>'id'
-);
-
-create policy read_own_permissions on auth.gateways_permissions to web
-using (
-	access::text = current_setting('request.jwt.claims', true)::json->>'id'
-);
-
-create policy read_own_permissions on auth.watchers_permissions to web
-using (
-	access::text = current_setting('request.jwt.claims', true)::json->>'id'
-);
-
 
 commit;
