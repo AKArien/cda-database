74e8175fd572260adc3a578298159d7a414a5aff
Hue <skeptikal.monke@gmail.com>
2026-07-11 17:31:20 +0200

    update tests list

--- tests.sql
+++ tests.sql
@@ -1,8 +1,12 @@
 \c cda;
 
-begin;
 create extension if not exists pgtap;
 
-\i tests/004_rls.sql
+\i tests/002-auth.sql
+\i tests/004-rls.sql
+\i tests/005-masking.sql
+\i tests/006-ingest.sql
+\i tests/007-create.sql
+\i tests/008-write.sql
 
-rollback;
+drop extension pgtap;
